2c9911a96dedd874f7488a62587d5ce842fab379
noreen <noreennelligancilms@gmail.com>
2023-02-07 08:24:00 +0000

    fix mixins but require more work

--- csiblog/views.py
+++ csiblog/views.py
@@ -41,19 +41,18 @@ class PostAddView(LoginRequiredMixin, CreateView):
     #     return super(PostAddView, self).form_valid(form)
 
 
-class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+# class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = "postupdate.html"
-    fields = ('name', 'contributor', 'date', 'content',)
+    # fields = ('name', 'contributor', 'date', 'content',)
     form_class = PostForm
     success_url = reverse_lazy('postread')
 
-    def get_object(self, queryset=None):
-        if self.request.user.is_authenticated:
-            queryset = Profile.objects.get(user=self.request.user)
-        return queryset
-    
-
+    # def get_object(self, queryset=None):
+    #     if self.request.user.is_authenticated:
+    #         queryset = Profile.objects.get(user=self.request.user)
+    #     return queryset
     # def test_func(self):
     #     return self.request.user == self.get_object().user
 
