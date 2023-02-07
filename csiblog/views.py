1f9c29d4c158d2e9c0dc6ca38995670d142827a7
noreen <noreennelligancilms@gmail.com>
2023-02-07 15:51:19 +0000

    Work on Loginmixins

--- csiblog/views.py
+++ csiblog/views.py
@@ -12,6 +12,21 @@ from .forms import PostForm
 
 
 # # Create your views here.
+class PostAddView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "postcreate.html"
+    # fields = ['name', 'slug', 'contributor', 'date', 'image', 'content', 'no_of_likes', 'excerpt', 'status']
+    form_class = PostForm
+    login_url = 'postread'
+    permission_denied_message = 'You are not allowed access here'
+    success_url = reverse_lazy('postread')
+
+    def form_valid(self, form):
+        form.instance.contributor = self.request.user
+        return super(PostAddView, self).form_valid(form)
+        if not request.user.is_authenticated:
+
+
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = "postupdate.html"
@@ -27,6 +42,7 @@ class PostDelete(DeleteView):
     model = Post
     template_name = "postdelete.html"
     success_url = reverse_lazy('postread')
+
     def test_func(self):
         return self.request.user == self.get_object().contributor
 
@@ -44,45 +60,3 @@ class PostDetailView(DetailView):
     #     post = get_object_or_404(queryset, slug=slug)
     model = Post
     template_name = "postdetail.html"
-    success_url = reverse_lazy('postread.html')
-
-
-class PostAddView(LoginRequiredMixin, CreateView):
-    model = Post
-    template_name = "postcreate.html"
-    # fields = ['name', 'slug', 'contributor', 'date', 'image', 'content', 'no_of_likes', 'excerpt', 'status']
-    form_class = PostForm
-    login_url = 'postread'
-    permission_denied_message = 'You are not allowed access here'
-    success_url = reverse_lazy('postread')
-
-    def form_valid(self, form):
-        form.instance.contributor = self.request.user
-        return super(PostAddView, self).form_valid(form)
-        if not request.user.is_authenticated:
-
-
-
-
-# def review(request):
-# messages.info("You need to be logged in in order to add a blog post")
-# class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-
-
-
-# def get_object(self, queryset=None):
-#    if self.request.user.is_authenticated:
-#       queryset = Profile.objects.get(user=self.request.user)
-#     return queryset
-
-
-    # def test_func(self):
-    #     # return contributor.request.contributor == self.get_object().contributor
-    #     form.instance.contributor = self.request.user
-    #     return super(PostAddView, self).form_valid(form)
-
-# def test_func(self):
-#     return 1 + 1 == 2
-
-
-
