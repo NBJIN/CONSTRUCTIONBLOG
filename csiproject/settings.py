ed8f99cb7bee71ce2b4cd93e583aca3e248de1f7
noreen <noreennelligancilms@gmail.com>
2023-02-11 21:39:10 +0000

    Update setting.py to display messages

--- csiproject/settings.py
+++ csiproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -153,6 +154,14 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 LOGIN_REDIRECT_URL = 'postread'
 LOGOUT_REDIRECT_URL = 'postread'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 
 # Default primary key field type
